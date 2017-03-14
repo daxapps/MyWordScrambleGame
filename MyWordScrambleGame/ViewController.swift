ea345a35045b53c99cc58d2273d7f32e752ac2cb
daxapps <daxapps777@gmail.com>
2017-03-14 11:54:54 -0500

    Setup and read from disk with contentsOfFile

--- MyWordScrambleGame/ViewController.swift
+++ MyWordScrambleGame/ViewController.swift
@@ -7,12 +7,53 @@
 //
 
 import UIKit
+import GameplayKit
 
-class ViewController: UIViewController {
+class ViewController: UITableViewController {
 
+    var allWords = [String]()
+    var usedWords = [String]()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        if let startWordsPath = Bundle.main.path(forResource: "start", ofType: "txt") {
+            
+            if let startWords = try? String(contentsOfFile: startWordsPath) {
+                
+                allWords = startWords.components(separatedBy: "\n")
+                
+            } else {
+                
+                allWords = ["silkworm"]
+            }
+        }
+        
+        startGame()
+    }
+    
+    func startGame() {
+        
+        allWords = GKRandomSource.sharedRandom().arrayByShufflingObjects(in: allWords) as! [String]
+        title = allWords[0]
+        usedWords.removeAll(keepingCapacity: true)
+        tableView.reloadData()
+        
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        
+        return usedWords.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Word", for: indexPath)
+        
+        cell.textLabel?.text = usedWords[indexPath.row]
+        
+        return cell
+        
     }
 
     override func didReceiveMemoryWarning() {
