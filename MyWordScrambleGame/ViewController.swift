bbc15850c2549881931ea4f63ee3babc2d3f9e81
daxapps <daxapps777@gmail.com>
2017-03-15 17:43:20 -0500

    Give User Feedback

--- MyWordScrambleGame/ViewController.swift
+++ MyWordScrambleGame/ViewController.swift
@@ -78,6 +78,8 @@ class ViewController: UITableViewController {
     func submit(answer: String) {
         
         let lowerAnswer = answer.lowercased()
+        let errorTitle: String
+        let errorMessage: String
         
         if isPossible(word: lowerAnswer) {
             if isOriginal(word: lowerAnswer) {
@@ -87,9 +89,26 @@ class ViewController: UITableViewController {
                     
                     let indexPath = IndexPath(row: 0, section: 0)
                     tableView.insertRows(at: [indexPath], with: .automatic)
+                    
+                    return
+                    
+                } else {
+                    errorTitle = "Word not recognized"
+                    errorMessage = "You can't just make them up, you know!"
                 }
+            } else {
+                errorTitle = "Word used already"
+                errorMessage = "Be more original!"
             }
+        } else {
+            errorTitle = "Word not possible"
+            errorMessage = "You can't spell that work from \(title!.lowercased())!"
         }
+        
+        let ac = UIAlertController(title: errorTitle, message: errorMessage, preferredStyle: .alert)
+        ac.addAction(UIAlertAction(title: "OK", style: .default))
+        
+        present(ac, animated: true)
         
     }
     
